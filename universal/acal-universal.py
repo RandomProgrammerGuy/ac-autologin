60288336b714286e616272ba43aff75780bbc826
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-20 18:03:09 +0330

    added browser drivers, backbone code and credentials file

--- universal/acal-universal.py
+++ universal/acal-universal.py
@@ -1,1 +1,12 @@
 from selenium import webdriver
+
+print('Welcome to AC Autologin. \n Please choose your browser first \n \n Use the legend below:\n C = Chrome, F = Firefox, S = Safari')
+browserchoice = input('Enter Browser Here')
+chromedriver = webdriver.Chrome('chromedriver')
+firefoxdriver = webdriver.Firefox('geckodriver')
+safaridriver = webdriver.Safari('safaridriver')
+if browserchoice == 'C':
+
+elif browserchoice == 'F':
+
+elif browserchoice == 'S':
